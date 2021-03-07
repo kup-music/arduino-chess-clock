aca8dfce9890e40a9a1eafabefec1f21c4f893be
Twalaght <jonathanchapman6799@gmail.com>
2021-03-07 22:16:47 +1030

    Added starting pause, misc stuff too

--- tg.ino
+++ tg.ino
@@ -10,27 +10,49 @@ const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  // Serial.println("Double 7 Segment Backpack Test");
-  matrix1.begin(0x70);
-  matrix2.begin(0x71);
+	// put your setup code here, to run once:
+	Serial.begin(9600);
+	// Serial.println("Double 7 Segment Backpack Test");
+	matrix1.begin(0x70);
+	matrix2.begin(0x71);
 
 
-  pinMode(8, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, INPUT);
-  digitalWrite(8, HIGH);
-  digitalWrite(9, LOW);
+	pinMode(8, OUTPUT);
+	pinMode(9, OUTPUT);
+	pinMode(10, INPUT);
+	digitalWrite(8, HIGH);
+	digitalWrite(9, LOW);
 
-  lcd.begin(16, 2);
-  // lcd.print("Hello, World!");
+	lcd.begin(16, 2);
+	// lcd.print("Hello, World!");
+
+
+	// TODO - Eventually can decide the "game name" based on input time, increment is not considered
+	/* if (time <= 2) { */
+	/* 	// Bullet */
+	/* } else if (time <= 10) { */
+	/* 	// Blitz */
+	/* } else if (time <= 15) { */
+	/* 	// Rapid */
+	/* } else if (time <= 30) { */
+	/* 	// Clasical */
+	/* } else { */
+	/* 	// Custom game */
+	/* } */
 }
 
-
+// Take a millisecond time code and format it to a displayable number
 long time_format(long time_code) {
-	long final = ((time_code / 1000 / 60) * 100) + (time_code / 1000 % 60);
-	return final;
+	long output = time_code / 1000;
+
+	// If player has less than 10 seconds switch to a precision display
+	if (output > 10) {
+		output = ((time_code / 1000) * 100) + (time_code % 100);
+	} else {
+		output = ((time_code / 1000 / 60) * 100) + (time_code / 1000 % 60);
+	}
+
+	return output;
 }
 
 
@@ -39,6 +61,15 @@ void loop() {
 	long totaltime = 300000;
 	long player_time[2] = {totaltime, totaltime};
 
+
+	// TODO - This won't actually display anything till the clock is hit
+	// but this whole segment needs a rewrite anyway, fix it later
+	// TODO - This "memory" can also later be used to have time increments when a player moves
+	// Get stuck in this loop until the button is hit for the first time
+	int latch = digitalRead(10);
+	while (true) {
+		if (digitalRead(10) != latch) break;
+	}
 
 	while (true) {
 		// Measure a unit of time for each run of the loop
